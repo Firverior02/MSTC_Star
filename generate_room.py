cfa8d9c859ccfd7ad2c8524dc13658fdade4ebc2
ALexander Runebou <alerun@kth.se>
2025-04-11 12:32:12 +0200

    chore: refactor, document and comment generate_room

--- generate_room.py
+++ generate_room.py
@@ -6,47 +6,82 @@ import networkx as nx
 
 from utils.nx_graph import nx_graph_write
 
+# Constants
 ROOMS_DIR = 'data/rooms/'
 
 def generate_room(x: int, y: int, density: float):
+    """Generate a room of dimensions x * y with density 
+
+    Args:
+        x (int): x dimension of room
+        y (int): y dimension of room
+        density (float): hom much of the room should be obstacles
+
+    Raises:
+        ValueError: Density must be in range [0, 1]
+        ValueError: x and y must be greater than 0
+    """
+    
+    # Argument validation
     if density < 0 or density > 1: 
-        raise Exception("Density must be in range [0,1]")
+        raise ValueError("Density must be in range [0,1]")
     
     if x < 1 or y < 1: 
-        raise Exception("x and y must be greater than 0")
+        raise ValueError("x and y must be greater than 0")
     
+    # Open/Create for room to be generated
     dir_path = f'{ROOMS_DIR}{int(density*100)}_ROOMS/{x}x{y}/'
-    
     os.makedirs(dir_path, exist_ok=True)
+    
+    # Count number of files in directory
     idx = sum(1 for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))) + 1
 
+    # Room file name
     f_path = f'ROOM_{x}x{y}_{int(density*100)}_{idx}.graph'
 
-
+    # Create default 2d grid of size x * y
     room = nx.grid_2d_graph(x, y)
+    nodes = list(room.nodes())
+    
+    # How many obstacles to generate
     num_obs = round(x * y * density)
 
-    nodes = list(room.nodes())
+    # Place obstacles
     for _ in range(num_obs):
+        # Choose a random node for the obstacle
         choice = random.choice(nodes)
 
-        while is_illegal(choice, room):
+        # Continue trying to place while invalid placement (graph is no longer connected)
+        while _is_illegal(choice, room):
             nodes.remove(choice)
             if len(nodes) == 0:
-                print('not good')
-                exit(0)
+                raise IndexError("No more nodes to assign obstacle to")
             choice = random.choice(nodes)
         
         room.remove_node(choice)
         nodes.remove(choice)
     
+    # Write room to file
     nx_graph_write(room, dir_path + f_path)
 
 
-def is_illegal(choice, room: nx.Graph):
-    temp = room.copy()
-    temp.remove_node(choice)
-    return not nx.is_connected(temp)
+    def _is_illegal(choice, room: nx.Graph):
+        """Checks if an obstacle placement is invalid
+
+        Args:
+            choice (node): The node that should become an obstacle
+            room (nx.Graph): The entire room graph
+
+        Returns:
+            bool: If the placement is invalid
+        """
+        
+        # Creates a copy of the room and removed node from it
+        temp = room.copy()
+        temp.remove_node(choice)
+        
+        # If graph is no longer connected, it is an invalid placement
+        return not nx.is_connected(temp)
 
 
-generate_room(20, 20, 0.2)
+#generate_room(20, 20, 0.2)
